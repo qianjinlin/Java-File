aa49fd5af5c6f10ea43964624303c130f8142627
钱金林 <58063815+qianjinlin@users.noreply.github.com>
2021-03-29 15:32:16 +0800

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,34 @@
+package com.test;
+
+import java.io.File;
+import java.util.Objects;
+
+public class Test {
+    private static int time = 0;
+    public static void main(String []args) {
+        File file  = new File("E://");
+        File[] fileList = file.listFiles();
+        for (int i = 0; i < Objects.requireNonNull(fileList).length; i++) {
+            checkFileAndDeleteNullDi(fileList[i].toString());
+
+
+        }
+
+    }
+
+    private static void checkFileAndDeleteNullDi(String str) {
+        File file = new File("E://");
+        File[] files = file.listFiles();
+        for (int i = 0; i < Objects.requireNonNull(files).length; i++) {
+            if(Objects.equals(files[i].toString(), str)){
+                File file1 = new File(str);
+                if (file1.length() <= 0 && file1.isDirectory()){
+                    time++;
+                    System.out.println("删除的个数:"+time);
+                    file1.delete();
+                }
+            }
+        }
+    }
+}
+
